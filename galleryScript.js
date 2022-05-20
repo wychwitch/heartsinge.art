d2c273ef5fded553c107b99a1674ea4655a95356
Wych <wychwitchcraft@gmail.com>
2022-05-20 02:04:54 -0500

    Basic html layout, image modal

---
+++ galleryScript.js
@@ -0,0 +1,37 @@
+const gallery = document.getElementById("gallery");
+
+const imgModal = document.getElementById("image-modal");
+
+const galleryArray = Array.from(gallery.childNodes).filter(
+  (child) => child.nodeName !== "#text"
+);
+
+const modalImg = imgModal.firstElementChild;
+
+const display = function (pic) {
+  const imgSrc = pic.firstElementChild.src;
+  console.log(imgSrc);
+  modalImg.src = imgSrc;
+
+  imgModal.classList.remove("hidden");
+
+  imgModal.addEventListener("click", closeImage);
+};
+
+const closeImage = function (target) {
+  if (target.target !== modalImg) {
+    modalImg.src = "";
+    imgModal.classList.add("hidden");
+  }
+};
+
+console.log(galleryArray);
+
+galleryArray.forEach((pic) => {
+  console.log(pic);
+  pic.addEventListener("click", function () {
+    display(pic);
+  });
+});
+
+//const img = pic.firstChild.src;
