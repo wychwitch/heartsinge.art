4aa5b3bf1904aa3ae860bce7ad085d45ea662f51
wych <wychwitchcraft@gmail.com>
2023-12-30 16:56:46 -0600

    fix tailwind colors

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,3 +1,7 @@
 <script>
     import "../app.css";
 </script>
+
+<main class="bg-background-c text-text-c">
+    <slot />
+</main>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,5 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<h1 class="text-primary-c">Welcome to SvelteKit</h1>
+<div class="bg-primary-c p-4">
+    <h1>Welcome to SvelteKit</h1>
+    <p>Wowwie</p>
+</div>
