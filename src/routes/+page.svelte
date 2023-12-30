e9e07c556a7c7de5d71bbaa74e2023f4d6b50072
wych <wychwitchcraft@gmail.com>
2023-12-30 17:50:22 -0600

    centering items

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,12 +2,10 @@
     export let data;
 </script>
 
-<div class="flex flex-col content-center">
+<div class="flex flex-col content-center items-center">
     <h1 class="text-primary-c text-center text-4xl">{data.title}</h1>
 
-    <div class="mx-50">
-        <img src={data.mainImg} class="w-20 inline" />
-    </div>
+    <img src={data.mainImg} class="w-20 inline" />
 
     <section class="bg-primary-50 p-4 m-10 rounded-lg">
         <h1 class="text-secondary-400 text-center text-4xl">
