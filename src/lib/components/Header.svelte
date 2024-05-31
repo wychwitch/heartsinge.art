819b539b807a0f74b9bab363d8f68bed37c2353f
wychwitch <wych@wychwit.ch>
2024-05-31 00:12:17 -0500

    auuaghg trying again

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -3,12 +3,8 @@
         class="bg-primary-100 w-10/12 h-16 flex flex-row justify-around items-center text-xl mt-4 rounded-lg"
     >
         <a
-            href="home"
+            href="/"
             class="border-b-4 border-l-4 border-accent-c px-1 rounded">home</a
-        >
-        <a
-            href="about"
-            class="border-b-4 border-l-4 border-accent-c px-1 rounded">about</a
         >
         <a
             href="3dwork"
@@ -21,13 +17,17 @@
             >2d previs</a
         >
         <a
+            href="photogrammetry"
+            class="border-b-4 border-l-4 border-accent-c px-1 rounded"
+            >photogrammetry</a
+        >
+        <a
             href="games"
             class="border-b-4 border-l-4 border-accent-c px-1 rounded">games</a
         >
         <a
-            href="photogrammetry"
-            class="border-b-4 border-l-4 border-accent-c px-1 rounded"
-            >photogrammetry</a
+            href="about"
+            class="border-b-4 border-l-4 border-accent-c px-1 rounded">about</a
         >
     </nav>
 </div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,9 +4,13 @@
 </script>
 
 <div class="flex flex-col items-center">
-    <h1 class="text-primary-c text-center text-5xl my-12">{data.title}</h1>
+    <div class="w-8/12 h-16 container overflow-hidden">
+        <img class="object-none object-bottom" src="/images/saltflats_banner.png">
+    </div>
+    <h1 class="text-primary-c text-center text-5xl my-4">{data.title}</h1>
+    <h3 class="text-secondary-400 text-center text-3xl font-bold">Lou Brown</h3>
 
-    <img src={data.mainImg} class="w-40 inline" />
+    <img src={data.mainImg} class="w-40 inline my-10" />
 
     <section class="bg-secondary-c p-4 my-10 rounded-lg w-8/12">
         <h1 class="text-secondary-400 text-center text-4xl">
